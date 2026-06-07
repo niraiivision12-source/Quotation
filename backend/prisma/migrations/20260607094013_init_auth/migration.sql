fced9a62f66bc20c85f8e4743336cec3f4c4155c
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2026-06-07 15:11:06 +0530

    feat: create user and role schema

---
+++ backend/prisma/migrations/20260607094013_init_auth/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "public"."UserRole" AS ENUM ('OWNER', 'SALESMAN', 'ATTENDANT', 'ACCOUNTANT');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."UserRole" NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
