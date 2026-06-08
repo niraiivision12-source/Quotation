048c3edfe7478bfda3da5dd3c0b673709976362e
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2026-06-08 17:30:01 +0530

    feat: setup tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 
   resolve: {
     alias: {
