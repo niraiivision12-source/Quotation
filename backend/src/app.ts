337b6d6d2a8b1bee3b8273762cd3ef834a90f41c
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2026-07-11 20:31:24 +0530

    refact:Changed the upload limit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,14 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({
+    limit: '10mb'
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: '10mb'
+}));
 
 app.use(devRequestTracer);
 
